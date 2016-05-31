3a6ae0fb9f49a10bf65fc1d908a21c0d8003008d
Duane Griffin <duaneg@dghda.com>
2016-05-31 23:53:59 +1200

    GUI: log to console and a log file

--- scorer_to_usebio/qt/__main__.py
+++ scorer_to_usebio/qt/__main__.py
@@ -1,17 +1,66 @@
-import sys
+import logging.config
+from pathlib import Path
+
+log_file = Path.home() / '.scorer_to_usebio' / 'ScorerConverter.log'
+
+def configure_logging():
+    from PyQt5 import QtCore
+
+    log_file.parent.mkdir(parents=True, exist_ok=True)
+
+    logging.config.dictConfig({
+        'version': 1,
+        'disable_existing_loggers': False,
+
+        'formatters': {
+            'brief': {
+                'format': '%(message)s',
+            },
+            'detailed': {
+                'format': '%(asctime)s %(module)-17s %(lineno)-4d %(levelname)-8s %(message)s',
+            },
+        },
+
+        'handlers': {
+            'console': {
+                'level':'DEBUG',
+                'formatter': 'brief',
+                'class': 'logging.StreamHandler',
+            },
+
+            'file': {
+                'level':'DEBUG',
+                'formatter': 'detailed',
+                'class': 'logging.handlers.RotatingFileHandler',
+                'filename': str(log_file),
+                'maxBytes': 1024 * 1024,
+                'backupCount': 1,
+            },
+        },
+
+        'root': {
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG',
+            'propagate': True
+        }
+    })
+
+    logging.debug('Logging to: %s', log_file)
 
 try:
+    import sys
     from . import ScorerConverter
     from PyQt5 import QtWidgets
 
     def main():
+        configure_logging()
         app = QtWidgets.QApplication(sys.argv)
         sc = ScorerConverter(app)
         sc.show()
         app.exec_()
-
-    if __name__ == "__main__":
-        main()
 except ImportError:
     def main():
         pass
+
+if __name__ == "__main__":
+    main()
